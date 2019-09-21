174a230d906432849c3a8b5e81f75fff29ad8e46
Mystic <mysteriousGE@gmail.com>
2019-09-21 16:36:48 +0800

    Pass UnitTest.

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -1,4 +1,6 @@
 ﻿using DependencyInjectionWorkshop.Models;
+using DependencyInjectionWorkshop.Repositories;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace DependencyInjectionWorkshopTests
@@ -9,7 +11,22 @@ namespace DependencyInjectionWorkshopTests
         [Test]
         public void is_valid()
         {
-            var authenticationService = new AuthenticationService();
+            var profile = Substitute.For<IProfile>();
+            var hash = Substitute.For<IHash>();
+            var otpService = Substitute.For<IOtpService>();
+            var notification = Substitute.For<INotification>();
+            var failedCounter = Substitute.For<IFailedCounter>();
+            var logger = Substitute.For<ILogger>();
+
+            profile.GetPassword("Mystic").Returns("hashed_password");
+            hash.Hash("password").Returns("hashed_password");
+            otpService.GetCurrentOtp("Mystic").Returns("123456");
+
+            var authenticationService = new AuthenticationService(profile, hash, otpService, notification, failedCounter, logger);
+
+            var isValid = authenticationService.Verify("Mystic", "password", "123456");
+
+            Assert.IsTrue(isValid);
         }
     }
 }
