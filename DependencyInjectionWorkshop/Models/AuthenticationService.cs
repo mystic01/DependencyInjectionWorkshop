43b72ef9eefe93ea40bbbfc3e81cc722c0156473
Mystic <mysteriousGE@gmail.com>
2019-09-21 15:28:38 +0800

    Move to file.

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -2,19 +2,6 @@
 
 namespace DependencyInjectionWorkshop.Models
 {
-    public class NLogAdapter
-    {
-        public NLogAdapter()
-        {
-        }
-
-        public void LogMessage(string message)
-        {
-            var logger = NLog.LogManager.GetCurrentClassLogger();
-            logger.Info(message);
-        }
-    }
-
     public class AuthenticationService
     {
         private readonly IProfile _profile;
