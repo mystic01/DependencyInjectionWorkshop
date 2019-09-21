9032622fa58c7d01c81af61bfc46be022cdcfc81
Mystic <mysteriousGE@gmail.com>
2019-09-21 10:19:48 +0800

    Failed. Notify via Slack.

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using Dapper;
+using SlackAPI;
 
 namespace DependencyInjectionWorkshop.Models
 {
@@ -44,6 +45,8 @@ namespace DependencyInjectionWorkshop.Models
             }
             else
             {
+                var slackClient = new SlackClient("my api token");
+                slackClient.PostMessage(response1 => { }, "my channel", "Filed to login.", "my bot name");
                 return false;
             }
         }
