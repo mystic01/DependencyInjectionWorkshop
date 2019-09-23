b26082f13d1031126141a5ae7e7363b8afa40627
Mystic <mysteriousGE@gmail.com>
2019-09-23 23:19:28 +0800

    Refactor unit test.

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -7,23 +7,45 @@ namespace DependencyInjectionWorkshopTests
     [TestFixture]
     public class AuthenticationServiceTests
     {
+        private const string DefaultAccountId = "mystic";
+        private const string DefaultHashedPassword = "hashed_password";
+        private const string DefaultPassword = "password";
+        private const string DefaultOtp = "123456";
+        private IProfile _profileDao;
+        private INotification _notification;
+        private IFailedCounter _failedCounter;
+        private IHash _hash;
+        private ILogger _logger;
+        private IOtpService _otpService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _profileDao = Substitute.For<IProfile>();
+            _notification = Substitute.For<INotification>();
+            _failedCounter = Substitute.For<IFailedCounter>();
+            _hash = Substitute.For<IHash>();
+            _logger = Substitute.For<ILogger>();
+            _otpService = Substitute.For<IOtpService>();
+        }
+
         [Test]
         public void is_valid()
         {
-            var profile = Substitute.For<IProfile>();
-            var notification = Substitute.For<INotification>();
-            var failedCounter = Substitute.For<IFailedCounter>();
-            var hash = Substitute.For<IHash>();
-            var logger = Substitute.For<ILogger>();
-            var otpService = Substitute.For<IOtpService>();
+            _profileDao.GetHashedPasswordFromDb(DefaultAccountId).Returns(DefaultHashedPassword);
+            _hash.Hash(DefaultPassword).Returns(DefaultHashedPassword);
+            _otpService.GetCurrentOtp(DefaultAccountId).Returns(DefaultOtp);
 
-            profile.GetHashedPasswordFromDb("mystic").Returns("hashed_password");
-            hash.Hash("password").Returns("hashed_password");
-            otpService.GetCurrentOtp("mystic").Returns("123456");
-
-            var authenticationService = new AuthenticationService(notification, failedCounter, logger, profile, hash, otpService);
-            var isValid = authenticationService.Verify("mystic", "password", "123456");
+            var isValid = WhenVerify();
             Assert.IsTrue(isValid);
+        }
+
+        private bool WhenVerify()
+        {
+            var authenticationService =
+                new AuthenticationService(_notification, _failedCounter, _logger, _profileDao, _hash, _otpService);
+            var isValid = authenticationService.Verify(DefaultAccountId, DefaultPassword, DefaultOtp);
+            return isValid;
         }
     }
 }
