7a54a4779a52d62795989daac75014c54ce52305
Mystic <mysteriousGE@gmail.com>
2019-09-24 23:20:13 +0800

    refactoring.

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -9,33 +9,21 @@ namespace DependencyInjectionWorkshop.Models
 
     public class AuthenticationService : IAuthentication
     {
-        private readonly IFailedCounter _failedCounter;
         private readonly IProfile _profileDao;
         private readonly IHash _sha256Adapter;
         private readonly IOtpService _otpService;
 
         public AuthenticationService(IFailedCounter failedCounter, IProfile profileDao, IHash sha256Adapter, IOtpService otpService)
         {
-            _failedCounter = failedCounter;
             _profileDao = profileDao;
             _sha256Adapter = sha256Adapter;
             _otpService = otpService;
         }
 
-        public AuthenticationService()
-        {
-            _failedCounter = new FailedCounter();
-            _profileDao = new ProfileDao();
-            _sha256Adapter = new Sha256Adapter();
-            _otpService = new OtpService();
-        }
-
         public bool Verify(string accountId, string password, string otp)
         {
             var passwordFromDb = _profileDao.GetHashedPasswordFromDb(accountId);
-
             var hashedPassword = _sha256Adapter.Hash(password);
-
             var currentOtp = _otpService.GetCurrentOtp(accountId);
 
             if (hashedPassword == passwordFromDb && otp == currentOtp)
